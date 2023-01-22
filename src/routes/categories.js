5ad23fd5c1c54bc6524efd92e57a287c26941d80
Henrique Sicilia <siciliahenrique@gmail.com>
2023-01-22 10:50:53 -0300

    feat: implement delete category

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,20 +1,38 @@
+const { response } = require('express');
 const express = require('express');
 const router = express.Router();
 const db = require("../db");
 
 
+const findOne = (id) => {
+  return query = {
+    name: "fetch-category",
+    text: "SELECT * FROM categories WHERE id = $1",
+    values: [Number(id)],
+}
+}
+
 router.get ("/", (req, res) => {
-  db.query("SELECT * FROM categories", (error, response) => {
+  try{
+    db.query("SELECT * FROM categories", (error, response) => {
     if(error) {
       return res.status(500).json(error);
     } 
     return res.status(200).json(response.rows);
   });
 
+  } catch(error) {
+    
+    return res.status(500).json(error);
+
+  }
+
 });
 
 router.post("/", (req, res) => {
-  const{ name } = req.body
+  
+  try{
+    const{ name } = req.body
 
   if(name.length < 3) {
     return res.status(400)
@@ -32,8 +50,38 @@ router.post("/", (req, res) => {
     return res.status(200).json(response.rows);
   });
 
+  } catch(error) {
+    return res.status(500).json(error);
+  }
+
   
 
+});
+
+router.delete('/:id', async (req, res) => {
+const { id } = req.params;
+  try {
+    if(!id) {
+  return res.status(400).json({ error: "Param idis mandatory."});
+} 
+  const query = findOne(id);
+  const category = await db.query(query);
+  if(!category.rows[0]) {
+    return res.status(404).json({ error: 'Category not found' });
+  }
+
+  const text = 'DELETE FROM categories WHERE id=$1 RETURNING *';
+  const values = [Number(id)];
+  const deleteResponse = await db.query(text, values);
+  if(!category.rows[0]) {
+    return res.status(400).json({ error: 'Category not deleted' });
+  }
+
+
+  return res.status(200).json(deleteResponse.rows);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 });
 
 module.exports = router;
